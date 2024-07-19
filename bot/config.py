760bddaa7b9acc17faedd5faad2bf2fbab3f9132
anikxpro2 <167355042+anikxpro2@users.noreply.github.com>
2024-07-19 15:17:31 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -22,7 +22,7 @@ try:
     DEV = 2067727121
     OWNER = 2067727121
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("THUMBNAIL")
+    THUMB = config("THUMBNAIL" , "https://graph.org/file/6d2a3f41687f9a67b18d8.jpg")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
